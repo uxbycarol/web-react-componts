3d2e6bde48338f3ba538d8419157a180253c1356
david cuspoca <cuspoca3d@gmail.com>
2021-06-16 21:08:08 -0600

    WebSite create with react and styled-components

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Container, Button} from '../../globalStyled';
+import {
+   InfoSec,
+   InfoRow,
+   InfoColumn,
+   TextWrapper,
+   TopLine,
+   Heading,
+   Subtitle,
+   ImgWrapper,
+   Img
+} from './InfoSection.elements';
+
+const InfoSection = ({lightBg, imgStart, lightTopLine, lightText, lightTextDesc, primary, buttonLabel, description, headLine, topLine, alt, img, start}) => {
+   return (
+      <>
+         <InfoSec lightBg={lightBg}>
+            <Container>
+               <InfoRow imgStart={imgStart}>
+                  <InfoColumn>
+                     <TextWrapper>
+                        <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                        <Heading lightText={lightText}>{headLine}</Heading>
+                        <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                        <Link to='/sign-up'>
+                           <Button big fontBig primary={primary}>
+                              {buttonLabel}
+                           </Button>
+                        </Link>
+                     </TextWrapper>
+                  </InfoColumn>
+                  <InfoColumn>
+                     <ImgWrapper start={start}>
+                        <Img src={img} alt={alt} />
+                     </ImgWrapper>
+                  </InfoColumn>
+               </InfoRow>
+            </Container>
+         </InfoSec>  
+      </>
+   )
+}
+
+export default InfoSection
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,68 @@
+import svg1 from '../../images/svg-1.svg';
+import svg2 from '../../images/svg-2.svg';
+import svg3 from '../../images/svg-3.svg';
+import profile from '../../images/profile.jpg';
+
+export const homeObjOne = {
+   lightBg: false,
+   primary : true,
+   imgStart: '',
+   lightTopLine: true,
+   lightTextDesc: true,
+   buttonLabel: 'Get Started',
+   description: 'We help business owners increase their revenue, Our team of unique specialist can help you acheive your business goals ',
+   headLine: 'Lead Generation Specialist for Online Business',
+   lightText: true,
+   topLine: 'Margeting Agency',
+   img: svg1,
+   start: '',
+   alt: 'Img',
+}
+
+export const homeObjTwo = {
+   lightBg: true,
+   primary : false,
+   imgStart: 'start',
+   lightTopLine: false,
+   lightTextDesc: false,
+   buttonLabel: 'Get Started',
+   description: 'We help business owners increase their revenue, Our team of unique specialist can help you acheive your business goals ',
+   headLine: 'Lead Generation Specialist for Online Business',
+   lightText: false,
+   topLine: 'Margeting Agency',
+   img: profile,
+   start: 'true',
+   alt: 'Img',
+}
+
+export const homeObjThree = {
+   lightBg: false,
+   primary : true,
+   imgStart: '',
+   lightTopLine: true,
+   lightTextDesc: true,
+   buttonLabel: 'Get Started',
+   description: 'We help business owners increase their revenue, Our team of unique specialist can help you acheive your business goals ',
+   headLine: 'Lead Generation Specialist for Online Business',
+   lightText: true,
+   topLine: 'Margeting Agency',
+   img: svg2,
+   start: '',
+   alt: 'Img',
+}
+
+export const homeObjFour = {
+   lightBg: false,
+   primary : true,
+   imgStart: 'start',
+   lightTopLine: true,
+   lightTextDesc: true,
+   buttonLabel: 'Get Started',
+   description: 'We help business owners increase their revenue, Our team of unique specialist can help you acheive your business goals ',
+   headLine: 'Lead Generation Specialist for Online Business',
+   lightText: true,
+   topLine: 'Margeting Agency',
+   img: svg3,
+   start: 'true',
+   alt: 'Img',
+}
